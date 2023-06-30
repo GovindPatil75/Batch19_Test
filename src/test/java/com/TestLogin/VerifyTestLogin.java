b2d5c4c1b28fda2a0593404c01d401790dfa83cd
GovindPatil75 <mulegr75@gmail.com>
2023-06-30 08:02:58 +0530

    Payment Test

--- src/test/java/com/TestLogin/VerifyTestLogin.java
+++ src/test/java/com/TestLogin/VerifyTestLogin.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
@@ -17,18 +18,11 @@ public class VerifyTestLogin {
 	public void verify_loginFB() {
 		
 		//Browser Open
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		WebDriverManager.edgedriver().setup();
+		EdgeDriver driver=new EdgeDriver();
 		driver.get("https://www.facebook.com/");
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
 		
-		//Identify
-		
-		LoginPom login=PageFactory.initElements(driver, LoginPom.class);
-		
-		login.gettxt_Email().sendKeys("Test@gmail.com");
-		login.getTxt_Password().sendKeys("12345678");
-		login.getBtn_login().click();
 		
 		
 		
---
+++ src/test/java/com/TestLogin/Verify_PaymentPageTest.java
@@ -0,0 +1,13 @@
+package com.TestLogin;
+
+import org.testng.annotations.Test;
+
+public class Verify_PaymentPageTest {
+
+	
+	@Test
+	public void verifyPaymentPageTest001() {
+		
+		System.out.println("Test");
+	}
+}
